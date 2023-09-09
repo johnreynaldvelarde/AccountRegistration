649f8dae7403ab4f62a6fe2bb77912c75b5268e6
Guze <Guze@LENOVO-PC>
2023-09-09 21:32:45 +0800

    tapos na

--- AccountRegistration/FrmRegistration.Designer.cs
+++ AccountRegistration/FrmRegistration.Designer.cs
@@ -87,6 +87,7 @@
             // 
             // cbProgram
             // 
+            this.cbProgram.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbProgram.FormattingEnabled = true;
             this.cbProgram.Location = new System.Drawing.Point(293, 68);
--- AccountRegistration/FrmRegistration.cs
+++ AccountRegistration/FrmRegistration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace AccountRegistration
 {
@@ -15,22 +16,62 @@ namespace AccountRegistration
         public FrmRegistration()
         {
             InitializeComponent();
+            listprogram();
+        }
+
+        public void listprogram()
+        {
+            ArrayList program = new ArrayList();
+            program.Add("BS in Computer Science");
+            program.Add("BS in Information Systems");
+            program.Add("BS in Information Technology ");
+            program.Add("BS in Computer Engineering");
+            program.Add("BS in Bussines Administration");
+            program.Add("BS in Accoutancy");
+            program.Add("BS in Management Accounting");
+
+            for (int i = 0; i< program.Count; i++) {
+                cbProgram.Items.Add(program[i]);
+            }
         }
 
         private void btnNext_Click(object sender, EventArgs e)
         {
-            StudentInfoClass.FirstName = txtFirstName.Text.ToString();
-            StudentInfoClass.LastName = txtLastName.Text.ToString();
-            StudentInfoClass.MiddleName = txtMiddleName.Text.ToString();
-            StudentInfoClass.Address = txtAddress.Text.ToString();
-            StudentInfoClass.Program = cbProgram.Text.ToString();
-            StudentInfoClass.Age = Convert.ToInt64(txtAge.Text.ToString());
-            StudentInfoClass.ContactNo = Convert.ToInt64(txtContactNo.Text.ToString());
-            StudentInfoClass.StudentNo = Convert.ToInt64(txtStudentNo.Text.ToString());
+            if (String.IsNullOrEmpty(txtStudentNo.Text) || String.IsNullOrEmpty(txtLastName.Text) || String.IsNullOrEmpty(txtFirstName.Text) || String.IsNullOrEmpty(txtMiddleName.Text) || 
+                String.IsNullOrEmpty(txtAge.Text) || String.IsNullOrEmpty(txtContactNo.Text) || String.IsNullOrEmpty(txtAddress.Text))
+            {
+                MessageBox.Show("Please fill in the blank");
+            }
+            else if (cbProgram.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a program");
+            }
+            else
+            {
+                StudentInfoClass.FirstName = txtFirstName.Text.ToString();
+                StudentInfoClass.LastName = txtLastName.Text.ToString();
+                StudentInfoClass.MiddleName = txtMiddleName.Text.ToString();
+                StudentInfoClass.Address = txtAddress.Text.ToString();
+                StudentInfoClass.Program = cbProgram.Text.ToString();
+                StudentInfoClass.Age = Convert.ToInt64(txtAge.Text.ToString());
+                StudentInfoClass.ContactNo = Convert.ToInt64(txtContactNo.Text.ToString());
+                StudentInfoClass.StudentNo = Convert.ToInt64(txtStudentNo.Text.ToString());
 
-            FrmConfrim frm = new FrmConfrim();
-            frm.ShowDialog();
+                FrmConfrim frm = new FrmConfrim();
+                DialogResult result = frm.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    txtFirstName.Text = "";
+                    txtLastName.Text = "";
+                    txtMiddleName.Text = "";
+                    txtAddress.Text = "";
+                    cbProgram.SelectedIndex = -1;
+                    txtAge.Text = "";
+                    txtContactNo.Text = "";
+                    txtStudentNo.Text = "";
+                }
+            }
         }
-        
     }
 }
